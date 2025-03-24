fcecb9be1d20cf4adb03dad41a0c8c9a8e9115e3
Alex <alexanderjrcalawag@gmail.com>
2025-03-24 10:56:02 +0800

    updated raw angular format

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from "@angular/common/http";
+import { AuthGuard } from './services/auth.guard';
 import { routes } from './app.routes';
-
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(
+    
+      withInterceptorsFromDi(),
+    ),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { IndexLayoutComponent } from './component/layout/index-layout/index-layout.component';
+import { UserLayoutComponent } from './component/layout/user-layout/user-layout.component';
+import { IndexComponent } from './views/index/index.component';
+import { AuthGuard } from './services/auth.guard';
+import { DashboardComponent } from './views/user/dashboard/dashboard.component';
+ 
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    {
+        path: '',
+        component: IndexLayoutComponent,
+        children: [
+            { path: '', component: IndexComponent, }, 
+        ],
+    },
+    {
+        path: 'user',
+        component: UserLayoutComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], data: {title: 'Dashboard'}},  
+            
+        ],
+    },
+    { path: '', redirectTo: '/', pathMatch: 'full' },
+    
+];
