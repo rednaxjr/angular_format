2d61b49982381b1954f762839fad52b7b577d02b
Alex <alexanderjrcalawag@gmail.com>
2025-03-24 10:47:48 +0800

    this is my raw angular format

---
+++ src/app/component/layout/index-layout/index-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-layout.component.html',
+  styleUrl: './index-layout.component.scss'
+})
+export class IndexLayoutComponent {
+
+}
